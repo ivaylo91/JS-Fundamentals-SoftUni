e9925625865c47a3954b805b263f070d10a432ba
ivaylo91 <ipenev91@gmail.com>
2021-04-26 18:38:33 +0300

    Print line of numbers and their sum

---
+++ syntax-exercises/printSum.js
@@ -0,0 +1,12 @@
+function printSum(start, end) {
+    let sum = 0;
+    let line = '';
+    for (let i = start; i <= end; i++) {
+        line += i + " ";
+        sum += i;
+    }
+    console.log(line);
+    console.log(`Sum: ${sum}`);
+}
+
+printSum(5, 10);
