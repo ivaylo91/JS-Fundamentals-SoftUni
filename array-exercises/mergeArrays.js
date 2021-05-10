6271607075aafe7cbcec78836a2917fa462fd2f1
ivaylo91 <ipenev91@gmail.com>
2021-05-10 14:09:10 +0300

    merge two arrays of string

--- array-exercises/mergeArrays.js
+++ array-exercises/mergeArrays.js
@@ -1,3 +1,17 @@
-function mergeArrays(arr1,arr2){
-
+function mergeArrays(arr1, arr2) {
+    let thirdArr = [];
+    for (let i = 0; i < arr1.length; i++) {
+        if (i % 2 === 0) {
+            let sum = Number(arr1[i]) + Number(arr2[i]);
+            thirdArr.push(sum);
+        } else {
+            let concat = arr1[i] + arr2[i];
+            thirdArr.push(concat);
+        }
+    }
+    console.log(thirdArr.join(" - "))
 }
+
+mergeArrays(['5', '15', '23', '56', '35'],
+    ['17', '22', '87', '36', '11']
+);
