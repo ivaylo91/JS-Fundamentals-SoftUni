e3fe28f1bb18998ae0f0ca3ceeedcb67d75551de
ivaylo91 <ipenev91@gmail.com>
2021-05-10 17:26:45 +0300

    rotate array of numbers

---
+++ array-exercises/array-rotation.js
@@ -0,0 +1,10 @@
+function rotate(arr, n) {
+    for (let i = 0; i < n; i++) {
+       let first = arr.shift();
+       arr.push(first);
+    }
+    console.log(arr.join(" "));
+}
+
+rotate([2, 4, 15, 31], 5)
+
